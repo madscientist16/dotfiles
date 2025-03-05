25196825f4dd5b343067429f05371e9812e15d29
Mad Scientist <67949699+madscientist16@users.noreply.github.com>
2025-03-05 17:18:13 +0500

    Remove LSP plugins and fix todo-comments for neovim

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -2,6 +2,7 @@
 
 local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
 
+-- mini.nvim
 now(function() require('mini.icons').setup() end)
 now(function() require('mini.statusline').setup() end)
 
@@ -18,82 +19,80 @@ now(function()
   vim.cmd.colorscheme 'catppuccin'
 end)
 
--- Highlight, edit, and navigate code
+-- Treesitter
 now(function()
   add({
     source = 'nvim-treesitter/nvim-treesitter',
     hooks = { post_checkout = function() vim.cmd('TSUpdate') end }
   })
   require('nvim-treesitter.configs').setup({
-    ensure_installed = { 'c', 'lua', 'vimdoc', 'markdown', 'markdown_inline' },
-
+    ensure_installed = {
+      'bash',
+      'fish',
+      'gitcommit',
+      'lua',
+      'markdown',
+      'markdown_inline',
+      'python',
+      'vimdoc'
+    },
     auto_install = true,
-
     highlight = { enable = true },
   })
 end)
 
--- Fuzzy Finder (files, lsp, etc)
+-- Telescope
 now(function()
   add({
     source = 'nvim-telescope/telescope.nvim',
     checkout = '0.1.x',
     depends = { 'nvim-lua/plenary.nvim' },
   })
-  -- See `:help telescope.builtin`
-  local builtin = require('telescope.builtin')
-  vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-  vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-  vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-  vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-  vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-  vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-  vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-  vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-  vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-  vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-
-  -- It's also possible to pass additional configuration options.
-  --  See `:help telescope.builtin.live_grep()` for information about particular keys
-  vim.keymap.set('n', '<leader>s/', function()
-    builtin.live_grep {
-      grep_open_files = true,
-      prompt_title = 'Live Grep in Open Files',
-    }
-  end, { desc = '[S]earch [/] in Open Files' })
-
-  -- Shortcut for searching your Neovim configuration files
-  vim.keymap.set('n', '<leader>sn', function()
-    builtin.find_files { cwd = vim.fn.stdpath 'config' }
-  end, { desc = '[S]earch [N]eovim files' })
 end)
 
--- LSP Plugins
-now(function()
-  add('williamboman/mason.nvim')
-  require('mason').setup()
-end)
+-- Telescope Configuration
+-- See `:help telescope.builtin`
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
-now(function()
-  add('williamboman/mason-lspconfig.nvim')
-  require('mason-lspconfig').setup()
-end)
+-- It's also possible to pass additional configuration options.
+--  See `:help telescope.builtin.live_grep()` for information about particular keys
+vim.keymap.set('n', '<leader>s/', function()
+  builtin.live_grep {
+    grep_open_files = true,
+    prompt_title = 'Live Grep in Open Files',
+  }
+end, { desc = '[S]earch [/] in Open Files' })
 
-now(function()
-  add('neovim/nvim-lspconfig')
-end)
+-- Shortcut for searching your Neovim configuration files
+vim.keymap.set('n', '<leader>sn', function()
+  builtin.find_files { cwd = vim.fn.stdpath 'config' }
+end, { desc = '[S]earch [N]eovim files' })
 
--- Highlight, list and search todo comments
+-- Todo Comments
 now(function()
   add({
     source = 'folke/todo-comments.nvim',
     depends = { 'nvim-lua/plenary.nvim' },
   })
+  require('todo-comments').setup({
+    signs = false,
+  })
 end)
 
--- Heuristically set buffer options
+-- vim-sleuth
 now(function() add('tpope/vim-sleuth') end)
 
+-- mini.nvim
 later(function() require('mini.comment').setup() end)
 later(function() require('mini.files').setup() end)
 later(function() require('mini.indentscope').setup() end)
@@ -128,7 +127,6 @@ later(function()
       { mode = 'n', keys = 's' },
       { mode = 'x', keys = 's' },
     },
-
     window = {
       config = {
         height = 10,
@@ -141,3 +139,5 @@ later(function()
     },
   })
 end)
+
+
