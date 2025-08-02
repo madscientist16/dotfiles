1bb14e2729fdddef52e97c10e799828abe7cc115
Mad Scientist <67949699+madscientist16@users.noreply.github.com>
2025-08-02 11:38:13 +0500

    Make neovim autosave when it loses foucs

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -1,8 +1,16 @@
 -- [ Autocommands ]
--- Highlight when yanking (copying) text.
 vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("hl-yank", { clear = true }),
   desc = "Highlight when yanking (copying) text",
   callback = function()
     vim.hl.on_yank()
+  end,
+})
+
+vim.api.nvim_create_autocmd("FocusLost", {
+  group = vim.api.nvim_create_augroup("autosave", { clear = true }),
+  desc = "Save file when Neovim loses foucs",
+  callback = function()
+    vim.cmd "update"
   end,
 })
