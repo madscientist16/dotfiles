8f1aedbecf6b3bffd9cc5c8b677743ad03804bd5
Mad Scientist <67949699+madscientist16@users.noreply.github.com>
2025-08-12 13:48:32 +0500

    Remove friendly-snippets

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -211,7 +211,7 @@ end)
 later(function()
   add {
     source = "saghen/blink.cmp",
-    depends = { "rafamadriz/friendly-snippets" },
+    -- depends = { "rafamadriz/friendly-snippets" },
     checkout = "v1.6.0",
   }
   require("blink.cmp").setup {
