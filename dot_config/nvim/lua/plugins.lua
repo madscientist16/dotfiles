2fba6fc02552e0cd98985c9bb4b176a742c51872
Mad Scientist <67949699+madscientist16@users.noreply.github.com>
2025-08-22 17:27:33 +0500

    feat: add hex color highlights to neovim

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -143,6 +143,9 @@ later(function()
       hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
       todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
       note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+
+      -- Highlight hex colors
+      hex_color = hipatterns.gen_highlighter.hex_color(),
     },
   }
 end)
